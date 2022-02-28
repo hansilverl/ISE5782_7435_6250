4f9c5bd7159148d58fb2968624941e5482c82159
Administrator <admin@example.com>
2022-02-28 18:23:57 +0200

    PR01

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,9 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * Representing complex shapes
+ */
 public interface Geometry {
     Vector getNormal(Point point);
 }
