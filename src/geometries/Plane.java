d7aa977fdd256356c90b74fb1560d20bdffa5ae6
Administrator <admin@example.com>
2022-02-28 18:23:57 +0200

    PR01

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -3,6 +3,9 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * Plane (defined by a point and the orthogonal vector).
+ */
 public class Plane implements Geometry {
     private Vector normal;
     private Point q0;
@@ -14,8 +17,16 @@ public class Plane implements Geometry {
         Vector norm=u.crossProduct(v);
 
         normal=norm.normalize();
+        q0=p1; //Arbitrary pick to represent
     }
 
+    // The second constructor, receives a point and a vector.
+    public Plane(Point pnt,Vector vec ) {
+        normal=vec.normalize();
+        q0=pnt;
+    }
+
+    //Returns normal
     public Vector getNormal() {
         return normal;
     }
--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -2,13 +2,18 @@ package primitives;
 
 import java.util.Objects;
 
+/**
+ * (a Euclidean point) a fundamental object in geometry -  a point with 3 coordinates.
+ * It will contain an object that holds three numbers aka it will have an attribute of type Double3 (it will use composition)
+ */
 public class Point {
-    final Double3 xyz;
+    final Double3 xyz;  //an object that holds three numbers (represents a point)
 
     public Point(double x, double y, double z) {
         xyz = new Double3(x, y, z);
     }
 
+    //Subtracting one point from another - returning a vector.
     public Vector subtract(Point p1) {
         Double3 res = new Double3(
                 xyz.d1 - p1.xyz.d1, xyz.d2 - p1.xyz.d2, xyz.d3 - p1.xyz.d3);
@@ -18,12 +23,13 @@ public class Point {
         return new Vector(res.d1, res.d2, res.d3);
     }
 
+    // Adding one point to another
     public Point add(Vector vec) {
         double x = xyz.d1 + vec.xyz.d1;
         double y = xyz.d2 + vec.xyz.d2;
         double z = xyz.d3 + vec.xyz.d3;
 
-        return new Point(x,y,z);
+        return new Point(x, y, z);
     }
 
     @Override
