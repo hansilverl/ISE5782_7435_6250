3a48ee013baecdaa8c13c918458855872dcb701d
Administrator <admin@example.com>
2022-03-07 16:38:15 +0200

    After class🥪🥪🥪🥪🥪

--- unitTests/primitives/PointTest.java
+++ unitTests/primitives/PointTest.java
@@ -2,20 +2,26 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class for {@link Point}
- * }
+ *
  */
 class PointTest {
 
+    Point p1 = new Point(1, 2, 3);
+
     @Test
     void testSubtract() {
+        assertEquals(new Vector (1,1,1),new Point(2,3,4).subtract(p1)
+                ,"ERROR: Point - Point does not work correctly");
     }
 
     @Test
     void testAdd() {
+        fail("not yet implemented hahahaha");
     }
 
     @Test
