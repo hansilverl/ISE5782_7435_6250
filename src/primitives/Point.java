abddeac0461a329aa90fa84c55bd06bf151a279e
buznach <hilabuznach5@gmail.com>
2022-03-07 15:23:41 +0200

    lesson3

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -2,34 +2,30 @@ package primitives;
 
 import java.util.Objects;
 
-/**
- * (a Euclidean point) a fundamental object in geometry -  a point with 3 coordinates.
- * It will contain an object that holds three numbers aka it will have an attribute of type Double3 (it will use composition)
- */
 public class Point {
-    final Double3 xyz;  //an object that holds three numbers (represents a point)
+    final Double3 _xyz;
 
     public Point(double x, double y, double z) {
-        xyz = new Double3(x, y, z);
+        _xyz = new Double3(x, y, z);
     }
 
-    //Subtracting one point from another - returning a vector.
+    public Point(Double3 xyz){_xyz = xyz;}
+
     public Vector subtract(Point p1) {
         Double3 res = new Double3(
-                xyz.d1 - p1.xyz.d1, xyz.d2 - p1.xyz.d2, xyz.d3 - p1.xyz.d3);
+                _xyz._d1 - p1._xyz._d1, _xyz._d2 - p1._xyz._d2, _xyz._d3 - p1._xyz._d3);
         if (Double3.ZERO.equals(res)) {
             throw new IllegalArgumentException("Subtractions results in zero - invalid");
         }
-        return new Vector(res.d1, res.d2, res.d3);
+        return new Vector(res._d1, res._d2, res._d3);
     }
 
-    // Adding one point to another
     public Point add(Vector vec) {
-        double x = xyz.d1 + vec.xyz.d1;
-        double y = xyz.d2 + vec.xyz.d2;
-        double z = xyz.d3 + vec.xyz.d3;
+        double x = _xyz._d1 + vec._xyz._d1;
+        double y = _xyz._d2 + vec._xyz._d2;
+        double z = _xyz._d3 + vec._xyz._d3;
 
-        return new Point(x, y, z);
+        return new Point(x,y,z);
     }
 
     @Override
@@ -37,18 +33,18 @@ public class Point {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return Objects.equals(xyz, point.xyz);
+        return Objects.equals(_xyz, point._xyz);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(xyz);
+        return Objects.hash(_xyz);
     }
 
     @Override
     public String toString() {
         return "Point{" +
-                "xyz=" + xyz +
+                "xyz=" + _xyz +
                 '}';
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -2,52 +2,43 @@ package primitives;
 
 import static primitives.Util.isZero;
 
-/**
- * In geometry, this is a fundamental object.
- * There is a size and a direction to it, defining our vector by the endpoint only,
- * The Vector class will derive from the Point class.
- */
 public class Vector extends Point {
     public Vector(double x, double y, double z) {
         super(x, y, z);
-        if (isZero(x) && isZero(y) && isZero(z)) {
+        if (isZero(x)&&isZero(y)&&isZero(z)) {
             throw new IllegalArgumentException("Zero vector invalid!");
         }
     }
 
-    //Calculating the vector length squared
     public double lengthSquared() {
-        return xyz.d1 * xyz.d1 + xyz.d2 * xyz.d2 + xyz.d3 * xyz.d3;
+        return _xyz._d1 * _xyz._d1 + _xyz._d2 * _xyz._d2 + _xyz._d3 * _xyz._d3;
     }
 
-    //Squaring the square of the vector length
     public double length() {
         return Math.sqrt(lengthSquared());
     }
 
-
-    // Takes two equal-length sequences of numbers , and returns a single number.
     public double dotProduct(Vector vec) {
-        double u1 = xyz.d1;
-        double u2 = xyz.d2;
-        double u3 = xyz.d3;
+        double u1 = _xyz._d1;
+        double u2 = _xyz._d2;
+        double u3 = _xyz._d3;
 
-        double v1 = vec.xyz.d1;
-        double v2 = vec.xyz.d2;
-        double v3 = vec.xyz.d3;
+        double v1 = vec._xyz._d1;
+        double v2 = vec._xyz._d2;
+        double v3 = vec._xyz._d3;
 
         return (u1 * v1 + u2 * v2 + u3 * v3);
     }
 
-    //Multiplying two vectors and their product is a new vector
-    public Vector crossProduct(Vector vec) {
-        double u1 = xyz.d1;
-        double u2 = xyz.d2;
-        double u3 = xyz.d3;
 
-        double v1 = vec.xyz.d1;
-        double v2 = vec.xyz.d2;
-        double v3 = vec.xyz.d3;
+    public Vector crossProduct(Vector vec) {
+        double u1 = _xyz._d1;
+        double u2 = _xyz._d2;
+        double u3 = _xyz._d3;
+
+        double v1 = vec._xyz._d1;
+        double v2 = vec._xyz._d2;
+        double v3 = vec._xyz._d3;
 
         return new Vector(
                 u2 * v3 - u3 * v2,
@@ -55,28 +46,27 @@ public class Vector extends Point {
                 u1 * v2 - u2 * v1);
     }
 
-    //Addition and subtraction of vectors
-    public Vector add(Vector vec) {
-        Double3 coordinate = new Double3(
-                xyz.d1 + vec.xyz.d1,
-                xyz.d2 + vec.xyz.d2,
-                xyz.d3 + vec.xyz.d3);
-        if (Double3.ZERO.equals(coordinate))    //
+    public Vector add(Vector vec)
+    {
+        Double3 coordinate =new Double3(
+                _xyz._d1 +vec._xyz._d1,
+                _xyz._d2 +vec._xyz._d2,
+                _xyz._d3 +vec._xyz._d3);
+        if (Double3.ZERO.equals(coordinate))
             throw new IllegalArgumentException("Zero vector invalid");
-        return new Vector(coordinate.d1, coordinate.d2, coordinate.d3);
+        return new Vector(coordinate._d1,coordinate._d2,coordinate._d3);
     }
 
-    //Scalar multiplication
-    public Vector scale(double scaleFactor) {
-        if (isZero(scaleFactor))
+    public Vector scale(double scaleFactor)
+    {
+        if (isZero(scaleFactor ))
             throw new IllegalArgumentException("Zero vector invalid!");
 
-        return (new Vector(xyz.d1 * scaleFactor, xyz.d2 * scaleFactor, xyz.d3 * scaleFactor));
+        return (new Vector(_xyz._d1 *scaleFactor, _xyz._d2 *scaleFactor, _xyz._d3 *scaleFactor));
     }
 
-    //Normalizing a vector
     public Vector normalize() {
-        double len = length();
-        return this.scale(1d / len);
+        double len=length();
+        return this.scale(1d/len);
     }
 }
