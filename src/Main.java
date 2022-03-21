3e89bc9e1a6aabfc1836392abb1db30409daf60d
buznach <hilabuznach5@gmail.com>
2022-03-21 16:02:39 +0200

    change of t2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import static primitives.Util.*;
 
 /*TODO:
  *  1. Tests that return in any of their function null (and getter function) i didn't write a test at all.
- *  2. Tests for: i didn't do tests for the function findIntersection
  *  3. In plane i didn't understand the functions getNormal so i didn't do test for them.
  *  4. Add comments
  *  5. Add javadoc to ray and maybe more 🤔 (Hila's smiley looks ugly😔😔)🧁🧁🧁🧁
