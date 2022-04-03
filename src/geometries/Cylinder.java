062a585b4658b3155abefb6840836a2e0f931343
Administrator <admin@example.com>
2022-04-03 21:36:24 +0300

    Cylinder

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,25 +1,29 @@
 package geometries;
 
+import primitives.Double3;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
 import java.util.List;
 
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
+
 /**
  * Class to represent a cylinder
  *
- *  Test class for {@link Cylinder}
- *
- *  @author Hila Buznach & Hannah Silverberg
- *
+ * @author Hila Buznach & Hannah Silverberg
  */
-public class Cylinder implements Geometry {
+public class Cylinder extends Tube {
     private final double _height;
 
-    public Cylinder(double height) {
+
+    //Constructor
+    public Cylinder(double height, Ray axisRay, double radius) {
+        super(axisRay, radius);
         _height = height;
-    }   //Constructor
+    }
 
     public double get_height() {
         return _height;
@@ -28,14 +32,27 @@ public class Cylinder implements Geometry {
     /**
      * implementing {@link Geometry#getNormal(Point)}
      * getter for normal vector
+     *
      * @param point
      * @return
      */
     @Override
     public Vector getNormal(Point point) {
-        return null;
+        Point p0 = getAxisRay().getP0();
+        Vector opp = getAxisRay().getDir();
+
+        if (p0.equals(point))
+            return opp;
+
+        Vector hypo = point.subtract(p0);
+        double t = hypo.dotProduct(opp);
+        if (isZero(t - _height) || isZero(t))          // point is on the bases of the cylinder
+            return opp;                        // axisRay vector
+
+        Point center = p0.add(opp.scale(t));
+        return point.subtract(center);
     }
-    
+
     @Override
     public String toString() {
         return "Cylinder{" +
@@ -45,6 +62,7 @@ public class Cylinder implements Geometry {
 
     /**
      * implementing {@link Intersectable#findIntersection(Ray)}
+     *
      * @param ray
      * @return
      */
---
+++ unitTests/geometries/CylinderTest.java
@@ -0,0 +1,37 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for {@link Cylinder}
+ *
+ * @author Hila Buznach & Hannah Silverberg
+ */
+
+class CylinderTest {
+
+    @Test
+    void testGetNormal() {
+        //Test when point is on the top of the cylinder)
+        Cylinder pl = new Cylinder(1d, new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 1d);
+        assertEquals(new Vector(0, 0, 1d), pl.getNormal(new Point(0, 0.5, 1)), "Error: Problem with cylinder normal calculation");
+                // Test where there is a simple single test here (when point is on the base of the cylinder)
+                assertEquals(new Vector(0, 0, 1d), pl.getNormal(new Point(0, 0.5, 0)), "Error: Problem with cylinder normal calculation");
+        // Test where point is on the surface of the cylinder)
+        assertEquals(new Vector(0, 1d, 0), pl.getNormal(new Point(0, 0.5, 0.5)), " Error: Problem with cylinder normal calculation");
+
+        assertNotEquals(pl.getNormal(new Point(0, 1, 0.00001)), pl.getNormal(new Point(0, 0.99999999, 0)), "Error: in boundary test with base !");
+        // Testing between surface and top of cylinder)
+        assertNotEquals(pl.getNormal(new Point(0, 1, 0.99999)), pl.getNormal(new Point(0, 0.99999999, 1)), "Error in boundary test with top !");
+
+    }
+
+    @Test
+    void testFindIntersection() {
+    }
+}
